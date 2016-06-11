85cb012b354abed23d9e867cb00a7c9ab7be0a06
Maximilian Algehed <m.algehed@gmail.com>
2016-06-12 00:44:06 +0200

    Some neat syntax for graphs

---
+++ CLDSL/GraphDSL.hs
@@ -0,0 +1,38 @@
+import Control.Monad.State
+import Data.Map
+import Data.Graph.Inductive hiding (mkNode)
+
+type Name = Node 
+
+data Sign = P | M | Q deriving (Ord, Eq, Show)
+
+type GraphSyntax a = State (Name, Gr Name Name, Map Name Sign) a
+
+infixl >+>
+infixl >->
+
+mkNode :: GraphSyntax Name
+mkNode = do
+            (i, gr, m) <- get
+            put (i+1, insNode (i, i) gr, m)
+            return i
+
+(>+>), (>->) :: GraphSyntax Name -> Name -> GraphSyntax Name
+g >+> w = do
+            v <- g
+            (i, gr, m) <- get
+            put (i+1, insEdge (v, w, i) gr, insert i P m)
+            return w
+g >-> w = do
+            v <- g
+            (i, gr, m) <- get
+            put (i+1, insEdge (v, w, i) gr, insert i M m)
+            return w
+
+link = return
+
+example = do
+            a <- mkNode
+            b <- mkNode
+            c <- mkNode
+            link a >+> b >+> c >+> a
